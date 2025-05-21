e15431094cf227c524b212eab55570935ffd49c3
Paul Schifferer <paul@schifferers.net>
2025-05-20 22:36:24 -0700

    Get Sentry DSN from environment

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,7 +3,7 @@ import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-	dsn: 'https://158f95b827004bf88ca068278f90f686@o126333.ingest.us.sentry.io/285522',
+	dsn: import.meta.env.SENTRY_DSN,
 
 	tracesSampleRate: 1.0,
 
