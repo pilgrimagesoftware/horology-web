fff3cc663e01cdc50ed192f724e67873fbcf8f05
Paul Schifferer <paul@schifferers.net>
2025-05-23 16:47:53 -0700

    Add TS config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel';
 
-const config = { kit: { adapter: adapter() } };
+const config = {
+    kit: { adapter: adapter() },
+    typescript: { types: ["node"] }
+};
 
 export default config;
